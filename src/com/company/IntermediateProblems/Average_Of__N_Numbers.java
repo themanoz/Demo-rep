2fb6b7a35acb398ec0bd63f1bd1da0788ec30322
Manoj Kumar <manojkumarbonala3@gmail.com>
2021-09-16 11:10:31 +0530

    My First Commit in Intellij

---
+++ src/com/company/IntermediateProblems/Average_Of__N_Numbers.java
@@ -0,0 +1,21 @@
+package com.company.IntermediateProblems;
+
+import java.util.Scanner;
+
+public class Average_Of__N_Numbers {
+    public static void main(String []args){
+        Scanner input=new Scanner(System.in);
+        int range,number,count=1,sum=0;
+        System.out.println("Enter the range:");
+        range=input.nextInt();
+        while (count <= range) {
+            System.out.println("Enter"+count+"number:");
+            number= input.nextInt();
+            sum=sum+number;
+            count++;
+        }
+        int average = sum / range;
+        System.out.println("Average of " +range + "Numbers is :"+average);
+
+    }
+}
